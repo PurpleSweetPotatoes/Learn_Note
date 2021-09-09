0b5ca663ba03c3af5a58b00d32af515c6775b846
MrBai <568604944@qq.com>
2021-09-09 14:51:09 +0800

    update

--- js_script/typeTocReset.js
+++ js_script/typeTocReset.js
@@ -1,12 +1,12 @@
-var content = document.getElementsByClassName('typora-export-content')[0];
+var content = document.getElementsByClassName("typora-export-content")[0];
 if (content != null) {
-    content.style = 'height: 100vh; display: flex';
+    content.style = "height: 100vh; display: flex";
 
-    var write = document.getElementById('write');
-    write.style = 'flex:1; overflow:auto';
+    var write = document.getElementById("write");
+    write.style = "flex:1; overflow:auto";
 
-    var toc = document.getElementsByClassName('md-toc')[0];
-    toc.style = 'overflow:auto';
+    var toc = document.getElementsByClassName("md-toc")[0];
+    toc.style = "overflow:auto";
 
     write.removeChild(toc);
     content.insertBefore(toc, write);
