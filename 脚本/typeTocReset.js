59578b61bcf92a863080945c13b8ac4182c5dea9
MrBai <568604944@qq.com>
2021-09-09 13:50:05 +0800

    update

---
+++ 脚本/typeTocReset.js
@@ -0,0 +1,15 @@
+<script>
+    var content = document.getElementsByClassName('typora-export-content')[0];
+    if (content != null) {
+        content.style = 'height: 100vh; display: flex';
+
+        var write = document.getElementById('write');
+        write.style = 'flex:1; overflow:auto';
+
+        var toc = document.getElementsByClassName('md-toc')[0];
+        toc.style = 'overflow:auto';
+
+        write.removeChild(toc);
+        content.insertBefore(toc, write);
+    } 
+</script>
